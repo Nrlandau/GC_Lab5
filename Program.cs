447ac22bc2bb8fd49c17cbbf2cc6e9a7f9d85e3a
Nicholas Landau <nrlandau@gmail.com>
2018-10-11 09:03:12 -0400

    created Factorial Function

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ namespace Lab5
 {
     class Program
     {
+        static int Factorial(int a)
+        {
+            if(a < 0)
+                throw new ArgumentOutOfRangeException();
+            if(a != 0)
+                return Factorial(a-1) * a;
+            return 1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
