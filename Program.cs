8bc9386b9f9cfa1fb825d271aa175affe7ab387a
Nicholas Landau <nrlandau@gmail.com>
2018-10-11 09:25:25 -0400

    Made Main Function

--- Program.cs
+++ Program.cs
@@ -4,16 +4,27 @@ namespace Lab5
 {
     class Program
     {
-        static int Factorial(int a)
+        static long Factorial(long factNum)
         {
-            if(a < 0)
+            if(factNum < 0)
                 throw new ArgumentOutOfRangeException();
-            if(a != 0)
-                return Factorial(a-1) * a;
+            if(factNum != 0)
+                return Factorial(factNum-1) * factNum;
             return 1;
         }
         static void Main(string[] args)
         {
+            //vars
+            long end_Number = 0,start_Number = 0;
+            //input
+            System.Console.WriteLine("Input a number from 0 to ?");
+            start_Number = int.Parse(System.Console.ReadLine());
+            //magic
+            end_Number = Factorial(start_Number);
+            //output
+            System.Console.WriteLine(end_Number);
+            
+            
         }
     }
 }
